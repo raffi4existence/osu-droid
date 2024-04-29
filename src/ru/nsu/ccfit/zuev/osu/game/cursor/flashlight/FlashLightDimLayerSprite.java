c41c3c6f06e32601e01a659bb79a945ce52f7efa
Louie Soulen Kurenai <168345503+LouieNotHere@users.noreply.github.com>
2024-04-29 12:20:24 +0800

    Update FlashLightDimLayerSprite.java

--- src/ru/nsu/ccfit/zuev/osu/game/cursor/flashlight/FlashLightDimLayerSprite.java
+++ src/ru/nsu/ccfit/zuev/osu/game/cursor/flashlight/FlashLightDimLayerSprite.java
@@ -7,7 +7,7 @@ import org.anddev.andengine.entity.modifier.AlphaModifier;
 
 
 public class FlashLightDimLayerSprite extends FlashlightAreaSizedSprite {
-    public final float BASE_SLIDER_DIM_ALPHA = 0.8f;
+    public final float BASE_SLIDER_DIM_ALPHA = 0f;
 
     public FlashLightDimLayerSprite() {
         super(TextureHelper.create1xRegion(Color.BLACK));
